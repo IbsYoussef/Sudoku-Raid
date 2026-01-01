0b73e95a9331a5cf3d420dcc7baca4a3f2c48a13
iyoussef <ibraheemyoussefiy@gmail.com>
2026-01-01 19:09:56 +0000

    feat: Implemented full sudoku board solving functionality in main.go

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main () {
-	
+import (
+	"fmt"
+	"os"
+	"sudoku/parser"
+	"sudoku/solver"
+	"sudoku/utils"
+)
+
+func main() {
+	// Get command-line arguments
+	args := os.Args[1:]
+
+	// Parse the arguments into a board
+	board, err := parser.ParseArgs(args)
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+
+	// Attempt to solve sudoku
+	if !solver.Solve(&board) {
+		fmt.Println("Error")
+		return
+	}
+
+	// Print the solved board
+	utils.PrintBoard(&board)
 }
